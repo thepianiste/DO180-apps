40a27793f5d599e9d06818f42b9e9635f6a9192a
thepianiste <118172212+thepianiste@users.noreply.github.com>
2022-11-16 14:55:00 +0100

    Update index.php

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,4 +1,4 @@
 <?php
 print "Hello, World! php version is " . PHP_VERSION . "\n";
-print "Hello, Ilyes! php version is " . PHP_VERSION . "\n";
+print "Hello, Ilyes! ouh ala sa3di ouh ala ayami " . PHP_VERSION . "\n";
 ?>
